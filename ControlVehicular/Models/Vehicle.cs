89f0ce6386421485437bba2e33aec3a1b40e3f6b
leg10nMX <nuke_mh@hotmail.com>
2014-01-22 13:01:40 -0600

    Adds Vehicle Model

---
+++ ControlVehicular/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlVehicular.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string UnitNumber { get; set; }
+        public int Year { get; set; }
+        public int Kilometrage { get; set; }
+    }
+}
