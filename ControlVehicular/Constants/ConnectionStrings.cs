676d1631bc85b880d4910b31b1ec07eb6d7dd51c
leg10nMX <nuke_mh@hotmail.com>
2014-01-22 17:12:39 -0600

    Adds ConnectionStrings  Adds migration Seed

---
+++ ControlVehicular/Constants/ConnectionStrings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlVehicular.Constants
+{
+    public class ConnectionStrings
+    {
+#if DEBUG
+        public const string DataBaseConnectionName = "EntitiesContext";
+#else
+        public const string DataBaseConnectionName = "DatabaseConnection";
+#endif
+    }
+}
--- ControlVehicular/Migrations/Configuration.cs
+++ ControlVehicular/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace ControlVehicular.Migrations
 {
+    using ControlVehicular.Constants;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web.Security;
+    using WebMatrix.WebData;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ControlVehicular.Models.EntitiesContext>
     {
@@ -15,6 +18,36 @@ namespace ControlVehicular.Migrations
         protected override void Seed(ControlVehicular.Models.EntitiesContext context)
         {
 
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection(ConnectionStrings.DataBaseConnectionName, "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            }
+
+            if (!Roles.RoleExists(RolesConstants.Administrator))
+            {
+                Roles.CreateRole(RolesConstants.Administrator);
+            }
+
+            if (!Roles.RoleExists(RolesConstants.DepartmentChief))
+            {
+                Roles.CreateRole(RolesConstants.Security);
+            }
+
+            if (!Roles.RoleExists(RolesConstants.DepartmentChief))
+            {
+                Roles.CreateRole(RolesConstants.Security);
+            }
+
+            string admin="Admin";
+            if (!WebSecurity.UserExists(admin))
+            {
+                WebSecurity.CreateUserAndAccount(admin, "nimdA", false);
+            }
+
+            if (!Roles.IsUserInRole(admin, RolesConstants.Administrator))
+            {
+                Roles.AddUserToRoles(admin, new string[] { RolesConstants.Administrator });
+            }
         }
     }
 }
